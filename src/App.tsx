8686ab2fe5fca5e920fd9b6e0216357d0d92918e
Jessica Campbell <jesscampb91@gmail.com>
2025-01-15 20:49:31 +0100

    Added html code for component SeatStatusLegend and imported to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.scss';
 import MoviePicker from './components/MoviePicker';
+import SeatStatusLegend from './components/SeatStatusLegend';
 
 function App() {
   return (
     <>
       <MoviePicker/>
+      <SeatStatusLegend/>
     </>
   );
 }
