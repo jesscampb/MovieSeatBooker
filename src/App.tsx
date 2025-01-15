c31fcd787b5a14e1ac577395f186c79d609d40a4
Jessica Campbell <jesscampb91@gmail.com>
2025-01-15 20:56:32 +0100

    Added html code for component SeatPicker and imported to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.scss';
 import MoviePicker from './components/MoviePicker';
 import SeatStatusLegend from './components/SeatStatusLegend';
+import SeatPicker from './components/SeatPicker';
 
 function App() {
   return (
     <>
       <MoviePicker/>
       <SeatStatusLegend/>
+      <SeatPicker/>
     </>
   );
 }
---
+++ src/components/SeatPicker.tsx
@@ -0,0 +1,69 @@
+function SeatPicker(){
+    return(
+        <div className="container">
+        <div className="screen"></div>
+        <div className="row">
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+        </div>
+        <div className="row">
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat occupied"></div>
+          <div className="seat occupied"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+        </div>
+        <div className="row">
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat occupied"></div>
+          <div className="seat occupied"></div>
+        </div>
+        <div className="row">
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+        </div>
+        <div className="row">
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat occupied"></div>
+          <div className="seat occupied"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+        </div>
+        <div className="row">
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat occupied"></div>
+          <div className="seat occupied"></div>
+          <div className="seat occupied"></div>
+          <div className="seat"></div>
+        </div>
+      </div>
+    );
+}
+
+export default SeatPicker;
