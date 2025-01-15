f9ad96c7d1b82c83b0990e5b44578de5c1ac7adb
Jessica Campbell <jesscampb91@gmail.com>
2025-01-15 14:17:18 +0100

    Installation fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import './App.css'
+import './App.css';
+import React from 'react';
 
 function App() {
-
   return (
     <>
-    
+
     </>
-  )
+  );
 }
 
-export default App
+export default App;
