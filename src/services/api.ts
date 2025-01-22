0a510ffce12b6a0b3253a0fef53d4952ec37a13a
Jessica Campbell <jesscampb91@gmail.com>
2025-01-22 22:33:40 +0100

    Removed "/movies" endpoint from apiUrl and added it manually to the call functions instead.

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,26 +2,26 @@ import axios from "axios";
 import { Movie } from "../models/Movie";
 
 
-const apiUrl = `http://localhost:3000/movies`;
+const apiUrl = `http://localhost:3000`;
 
 export async function getAllMovies() {
-    const response = await axios.get(apiUrl);
+    const response = await axios.get(`${apiUrl}/movies`);
     const movies = await response.data;
     return movies;
 }
 
 export async function getMovie(id:string) {
-    const response = await axios.get(`${apiUrl}/${id}`);
+    const response = await axios.get(`${apiUrl}/movies/${id}`);
     const movie = await response.data;
     return movie;
 }
 
 export async function addMovie(newMovie: Omit<Movie, 'id'>) {
-    const response = await axios.post(apiUrl, newMovie);
+    const response = await axios.post(`${apiUrl}/movies`, newMovie);
     return response.data;
 }
 
 export async function updateMovie(editedMovie: Movie) {
-    const response = await axios.put(`${apiUrl}/${editedMovie.id}`, editedMovie);
+    const response = await axios.put(`${apiUrl}/movies/${editedMovie.id}`, editedMovie);
     return response.data;
 }
