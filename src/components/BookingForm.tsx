0ed0e27d441aa19a6fd6cc655e9e30f13d9395d9
Jessica Campbell <jesscampb91@gmail.com>
2025-01-31 14:44:23 +0100

    Created event handler for submitting the form in BookingForm. Some Formik code started on.

--- src/components/BookingForm.tsx
+++ src/components/BookingForm.tsx
@@ -1,7 +1,7 @@
 import './BookingForm.scss';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
-import axios from 'axios';
+import { addBooking } from '../services/api';
 
 const BookingSchema = Yup.object().shape({
   fullName: Yup.string()
@@ -11,7 +11,18 @@ const BookingSchema = Yup.object().shape({
   phone: Yup.string()
     .matches(/^\+?\d{7,15}$/, 'Invalid phone number format')
     .required('Phone number is required')
-})
+});
+
+const handleBookingSubmit = async (values: {fullName: string; phone: string;}) => {
+  const newBooking = {
+    fullName: values.fullName,
+    phone: values.phone,
+    movieId: '',
+    seatId: ['']
+  };
+
+  await addBooking(newBooking);
+}
 
 function BookingForm() {
   return(
@@ -19,6 +30,16 @@ function BookingForm() {
       <p className='booking-prompt'>
         Please, enter your contact information to proceed with the booking.
       </p>
+
+      <Formik 
+        initialValues={{
+          fullName: '',
+          phone: '',
+        }}
+        validationSchema={BookingSchema}
+        onSubmit={handleBookingSubmit}
+      ></Formik>
+
       <form action='' className='form-container'>
         <div className='form-name'>
           <label htmlFor='fullName'>Name:</label>
@@ -30,6 +51,7 @@ function BookingForm() {
         </div>
         <button type='submit' className='form-button'>Confirm</button>
       </form>
+
     </div>
   );
 }
