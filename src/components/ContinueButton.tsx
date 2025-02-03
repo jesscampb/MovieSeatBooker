f248b6a95457afa93338330a1d22128cabf4d96c
Jessica Campbell <jesscampb91@gmail.com>
2025-02-03 16:08:18 +0100

    useState readyToBook is set to 'true' when ContinueButton is clicked.

--- src/components/ContinueButton.tsx
+++ src/components/ContinueButton.tsx
@@ -1,10 +1,18 @@
 import './ContinueButton.scss';
 
-function ContinueButton() {
+interface ContinueButtonProps {
+  setReadyToBook: (value: boolean) => void;
+}
+
+function ContinueButton({setReadyToBook}: ContinueButtonProps) {
+
+  const handleButtonClick = () => {
+    setReadyToBook(true);
+  }
 
   return(
     <div className='button-container'>
-      <button className='primary-button'>
+      <button className='primary-button' onClick={handleButtonClick}>
         Continue with booking
       </button>
     </div>
